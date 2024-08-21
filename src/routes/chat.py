ce328173ce4a79584281179e976fc13bf6534a0a
dkp1903 <dushyantpathak6789@gmail.com>
2024-08-21 19:43:43 +0000

    ART-2 chat uuid

--- src/routes/chat.py
+++ src/routes/chat.py
@@ -1,20 +1,36 @@
 import os
-from fastapi import APIRouter, FastAPI, WebSocket,  Request
+from fastapi import APIRouter, FastAPI, WebSocket, WebSocketDisconnect, Request, Depends, HTTPException
+import uuid
+from ..socket.connection import ConnectionManager
+from ..socket.utils import get_token
+
 
 chat = APIRouter()
+
+manager = ConnectionManager()
 
 # @route   POST /token
 # @desc    Route to generate chat token
 # @access  Public
 
+
 @chat.post("/token")
-async def token_generator(request: Request):
-    return None
+async def token_generator(name: str, request: Request):
+    token = str(uuid.uuid4())
+
+    if name == "":
+        raise HTTPException(status_code=400, detail={
+            "loc": "name",  "msg": "Enter a valid name"})
+
+    data = {"name": name, "token": token}
+
+    return data
 
 
 # @route   POST /refresh_token
 # @desc    Route to refresh token
 # @access  Public
+
 
 @chat.post("/refresh_token")
 async def refresh_token(request: Request):
@@ -26,5 +42,13 @@ async def refresh_token(request: Request):
 # @access  Public
 
 @chat.websocket("/chat")
-async def websocket_endpoint(websocket: WebSocket = WebSocket):
-    return None
+async def websocket_endpoint(websocket: WebSocket, token: str = Depends(get_token)):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            print(data)
+            await manager.send_personal_message(f"Response: Simulating response from the GPT service", websocket)
+
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
